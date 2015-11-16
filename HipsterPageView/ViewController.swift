4325c39d20ffc30a2c49eee19a7a86eb20fbffc5
Calvin Nisbet <calvinnisbet@calvins-macbook-air.local>
2015-11-15 22:35:16 -0800

    Initial Commit

---
+++ HipsterPageView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HipsterPageView
+//
+//  Created by Calvin Nisbet on 2015-11-15.
+//  Copyright © 2015 Lone Pine Creative. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
